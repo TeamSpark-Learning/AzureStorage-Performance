93ecbbb8af8ac73217775269d35fff93bd1d70e7
Boyko Ant <boyko.ant@live.com>
2017-02-05 19:39:06 +0200

    started with DownloadSinglePageFileSingleThread scenario

---
+++ dotNet-core/AzPerf/AzPerf.Storage/Blob/DownloadSinglePageFileSingleThread.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace AzPerf.Storage.Blob
+{
+    public class DownloadSinglePageFileSingleThread : StoragePerformanceBase
+    {
+        protected override async Task InitializeAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override async Task DoWorkAsync()
+        {
+            await Task.Delay(500);
+        }
+
+        protected override async Task CleanupAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
