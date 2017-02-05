528ad51cdbab4cc9dc5b134523fe273472c09bcb
Boyko Ant <boyko.ant@live.com>
2017-02-05 19:39:31 +0200

    started with DownloadSinglePageFileMultipleThreads scenario

---
+++ dotNet-core/AzPerf/AzPerf.Storage/Blob/DownloadSinglePageFileMultipleThreads.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace AzPerf.Storage.Blob
+{
+    public class DownloadSinglePageFileMultipleThreads : StoragePerformanceBase
+    {
+        protected override async Task InitializeAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override async Task DoWorkAsync()
+        {
+            await Task.Delay(500);
+        }
+
+        protected override async Task CleanupAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
