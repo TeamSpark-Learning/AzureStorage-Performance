85f959d93a2cb75baf400148d3488aad2f7ff7a0
Boyko Ant <boyko.ant@live.com>
2017-02-05 16:21:31 +0200

    added storage performance base class

---
+++ dotNet-core/AzPerf/AzPerf.Storage/StoragePerformanceBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace AzPerf.Storage
+{
+    public abstract class StoragePerformanceBase
+    {
+        protected abstract Task InitializeAsync();
+        protected abstract Task DoWorkAsync();
+        protected abstract Task CleanupAsync();
+
+        public async Task RunAsync()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Initializing...");
+            await CleanupAsync();
+            await InitializeAsync();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Initialized.");
+
+            var timer = new Stopwatch();
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Working...");
+            timer.Start();
+
+            await DoWorkAsync();
+
+            timer.Stop();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Finished.");
+
+            Console.WriteLine("Total time: {0}", timer.Elapsed);
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Cleaning up...");
+            await CleanupAsync();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Done.");
+        }
+    }
+}
